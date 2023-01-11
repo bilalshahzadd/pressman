b83e3cb75f397e84f1ab2ae4356969c8bfeeeae9
bilalshahzadd <bilalshahzad521@gmail.com>
2023-01-12 03:59:19 +0500

    maintaining folder

---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Loader extends Component {
+    render() {
+        return (
+            <div>Loader</div>
+        )
+    }
+}
+
+export default Loader
---
+++ src/components/News.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class News extends Component {
+    render() {
+        return (
+            <div>News</div>
+        )
+    }
+}
+
+export default News
