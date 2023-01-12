11feef7251aea95a639896fec084ac76bed099e3
bilalshahzadd <bilalshahzad521@gmail.com>
2023-01-12 05:43:50 +0500

    navbar and card update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
+import Header from './components/Header'
+import News from './components/News'
 
 class App extends Component {
   render() {
     return (
       <>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+        <Header />
+        <News />
       </>
     )
   }
--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
+import NewsItem from './NewsItem'
 
 class News extends Component {
+
+    image = "https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__340.jpg"
+
     render() {
         return (
-            <div>News</div>
+            <>
+                <h1 className="text-3xl font-bold text-center mt-5">Pressman - Watch Latest News Daily For Free</h1>
+                <div className="flex justify-between px-5 mt-5">
+                    <NewsItem imageLink={this.image} />
+                    <NewsItem imageLink={this.image} />
+                    <NewsItem imageLink={this.image} />
+                </div>
+            </>
         )
     }
 }
