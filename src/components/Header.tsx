bf20af22cbf64434152c6f078bb08d637aef72f7
bilalshahzadd <bilalshahzad521@gmail.com>
2023-01-25 04:23:07 +0500

    adding categories

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,10 +22,25 @@ class Header extends Component {
                                     <a className="nav-link active" aria-current="page" href="/about">Home</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">Features</a>
+                                    <a className="nav-link" href="/about">General</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">Pricing</a>
+                                    <a className="nav-link" href="/about">Entertainment</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/about">Sports</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/about">Health</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/about">Business</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/about">Science</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/about">Technology</a>
                                 </li>
                             </ul>
                         </div>
